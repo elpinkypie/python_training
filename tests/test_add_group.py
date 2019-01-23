310f81503333889bd1d1b2fa4e4d67edcea66919
elena <elena.dobranitsa@gmail.com>
2019-01-23 23:23:42 +0200

    test add group: deleted verification errors & base url to katalon

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -62,7 +62,6 @@ class UntitledTestCase(unittest.TestCase):
     
     def tearDown(self):
         self.driver.quit()
-        self.assertEqual([], self.verificationErrors)
 
 if __name__ == "__main__":
     unittest.main()
