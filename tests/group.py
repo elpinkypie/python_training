21bdc11096a89404f703f67c5cca5dda727633a6
elena <elena.dobranitsa@gmail.com>
2019-01-27 22:21:04 +0200

    new class where new contact info parameters would be passed

--- tests/group.py
+++ tests/group.py
@@ -4,3 +4,22 @@ class Group:
         self.name = name
         self.header = header
         self.footer = footer
+
+
+class ContactFormAttributes:
+
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, home, mobile, email, byear, phone2, notes, address2):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.mobile = mobile
+        self.email = email
+        self.byear = byear
+        self.phone2 = phone2
+        self.notes = notes
+        self.address2 = address2
+
--- tests/test_add_new_contact.py
+++ tests/test_add_new_contact.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
+from group import ContactFormAttributes
 
 class AddNewContact(unittest.TestCase):
     def setUp(self):
@@ -26,72 +23,72 @@ class AddNewContact(unittest.TestCase):
     def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/index.php")
 
-    def fill_first_last_name(self, wd):
+    def fill_first_last_name(self, wd, ct):
         wd.find_element_by_link_text("add new").click()
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("elena")
+        wd.find_element_by_name("firstname").send_keys(ct.firstname)
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys("test")
+        wd.find_element_by_name("middlename").send_keys(ct.middlename)
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("lastname")
+        wd.find_element_by_name("lastname").send_keys(ct.lastname)
 
-    def fill_nickname_title_company(self, wd):
+    def fill_nickname_title_company(self, wd, ct):
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys("nickname")
+        wd.find_element_by_name("nickname").send_keys(ct.nickname)
         wd.find_element_by_name("title").click()
         wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys("QA")
+        wd.find_element_by_name("title").send_keys(ct.title)
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys("CPA")
+        wd.find_element_by_name("company").send_keys(ct.company)
 
-    def fill_address(self, wd):
+    def fill_address(self, wd, ct):
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("no address")
+        wd.find_element_by_name("address").send_keys(ct.address)
 
-    def fill_home_work_phones(self, wd):
+    def fill_home_work_phones(self, wd, ct):
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys("+7 (495) 510-55-57")
+        wd.find_element_by_name("home").send_keys(ct.mobile)
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys("+7 (495) 510-55-57")
+        wd.find_element_by_name("mobile").send_keys(ct.mobile)
         wd.find_element_by_name("work").click()
         wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys("+7 (495) 510-55-57")
+        wd.find_element_by_name("work").send_keys(ct.mobile)
 
-    def fill_email(self, wd):
+    def fill_email(self, wd, ct):
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys("elena.dobranitsa@gmail.com")
+        wd.find_element_by_name("email").send_keys(ct.email)
 
-    def fill_birthday(self, wd):
+    def fill_birthday(self, wd, ct):
         wd.find_element_by_name("bday").click()
         Select(wd.find_element_by_name("bday")).select_by_visible_text("28")
         wd.find_element_by_name("bmonth").click()
         Select(wd.find_element_by_name("bmonth")).select_by_visible_text("August")
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys("1991")
+        wd.find_element_by_name("byear").send_keys(ct.byear)
 
     def button_enter_click(self, wd):
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
-    def fill_phone2_notes(self, wd):
+    def fill_phone2_notes(self, wd, ct):
         wd.find_element_by_name("phone2").click()
         wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys("Kiev")
+        wd.find_element_by_name("phone2").send_keys(ct.phone2)
         wd.find_element_by_name("notes").click()
         wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys("no notes")
+        wd.find_element_by_name("notes").send_keys(ct.notes)
 
-    def fill_address2(self, wd):
+    def fill_address2(self, wd, ct):
         wd.find_element_by_name("address2").click()
         wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys("no secondary address")
+        wd.find_element_by_name("address2").send_keys(ct.address)
 
 
     def logout(self, wd):
@@ -101,19 +98,19 @@ class AddNewContact(unittest.TestCase):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.fill_first_last_name(wd)
-        self.fill_nickname_title_company(wd)
+        self.fill_first_last_name(wd, ContactFormAttributes(firstname="elena", middlename="test", lastname="lastname"))
+        self.fill_nickname_title_company(wd, ContactFormAttributes(nickname="nickname", title="QA", company="CPA"))
 
-        self.fill_address(wd)
-        self.fill_home_work_phones(wd)
+        self.fill_address(wd, ContactFormAttributes(address="no address"))
+        self.fill_home_work_phones(wd, ContactFormAttributes(mobile="+7 (495) 510-55-57"))
 
-        self.fill_email(wd)
+        self.fill_email(wd, ContactFormAttributes(email="elena.dobranitsa@gmail.com"))
 
-        self.fill_birthday(wd)
+        self.fill_birthday(wd, ContactFormAttributes(byear="1991"))
 
-        self.fill_address2(wd)
+        self.fill_address2(wd, ContactFormAttributes(address="no secondary address"))
 
-        self.fill_phone2_notes(wd)
+        self.fill_phone2_notes(wd, ContactFormAttributes(phone2="Kiev", notes="no notes"))
         self.button_enter_click(wd)
         self.logout(wd)
 
