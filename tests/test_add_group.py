bb83980dc60d632ffd959cf760ba38f9308826eb
elena <elena.dobranitsa@gmail.com>
2019-01-28 23:20:02 +0200

    import fixed

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -1,21 +1,23 @@
-# -*- coding: utf-8 -*-
 import pytest
-from group import Group
-from application import Application
+from git.tests.group import Group
+from git.tests.application import Application
+
 
 @pytest.fixture
-def app(request):
+def fixt(request):
     fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
 
-def test_add_new_group(app):
-    app.login(username="admin", password="secret")
-    app.create_new_group(Group(name="gergerg", header="scsdcvs", footer="vsdvsv"))
-    app.logout()
 
-def test_add_emty_group(app):
-    app.login(username="admin", password="secret")
-    app.open_groups_page()
-    app.logout()
+def test_add_new_group(fixt):
+    fixt.login(username="admin", password="secret")
+    fixt.create_new_group(Group(name="gergerg", header="scsdcvs", footer="vsdvsv"))
+    fixt.logout()
+
+
+def test_add_emty_group(fixt):
+    fixt.login(username="admin", password="secret")
+    fixt.open_groups_page()
+    fixt.logout()
 
