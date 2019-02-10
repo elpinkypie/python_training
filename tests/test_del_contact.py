865a90ca30ac4cde688c8fa74143579f2a9523a2
elena <elena.dobranitsa@gmail.com>
2019-02-10 11:37:29 +0200

    fixed wait for message + EC changed to visibility of element to be located

--- tests/test_del_contact.py
+++ tests/test_del_contact.py
@@ -1,6 +1,6 @@
 
 
-def test_delete_first_group(fixt):
+def test_delete_first_contact(fixt):
     fixt.session.login(username="admin", password="secret")
     fixt.contact.delete_first_contact()
     fixt.session.logout()
