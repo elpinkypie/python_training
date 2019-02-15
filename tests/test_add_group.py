cb80b8d84296966accf59897a8faab7d6e8c77fa
elena <elena.dobranitsa@gmail.com>
2019-02-15 22:22:24 +0200

    creating lists of groups, added assert to compare default with changed quantity of groups

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -2,9 +2,14 @@ from git.model.group import Group
 
 
 def test_add_new_group(fixt):
+    old_groups = fixt.group.get_group_list()
     fixt.group.create(Group(name="gergerg", header="scsdcvs", footer="vsdvsv"))
+    new_groups = fixt.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 
 def test_add_empty_group(fixt):
+    old_groups = fixt.group.get_group_list()
     fixt.group.create(Group(name="", header="", footer=""))
-
+    new_groups = fixt.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
