233d3e41a36a1f8b26e5823ffd8adc37ade1878a
elena <elena.dobranitsa@gmail.com>
2019-02-15 23:07:25 +0200

    fixture finalizer issues

--- tests/test_edit_group.py
+++ tests/test_edit_group.py
@@ -4,6 +4,5 @@ from git.model.group import Group
 def test_edit_group(fixt):
     if fixt.group.count() == 0:
         fixt.group.create(Group(name="test"))
-
     fixt.group.edit_first_group(Group(name="editgroup1", header="group2", footer="group3"))
 
