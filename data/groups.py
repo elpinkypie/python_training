03b6f0c5c39209cdf17316d436f742812f91db19
elena <elena.dobranitsa@gmail.com>
2019-02-24 22:30:08 +0200

    Added dynamic tests generation

---
+++ data/groups.py
@@ -0,0 +1,9 @@
+from git.model.group import Group
+
+
+testdata = [
+    Group(name="", header="", footer=""),
+    Group(name="name1", header="header1", footer="footer1"),
+    Group(name="name2", header="header2", footer="footer2")
+]
+
--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -1,10 +1,8 @@
 from git.model.group import Group
-import pytest
-from git.data.add_group import constant as test_data
 
 
-@pytest.mark.parametrize("group", test_data, ids=[repr(x) for x in test_data])
-def test_add_new_group(fixt, group):
+def test_add_new_group(fixt, data_groups):
+    group = data_groups
     old_groups = fixt.group.get_group_list()
     fixt.group.create(group)
     assert len(old_groups) + 1 == fixt.group.count()
